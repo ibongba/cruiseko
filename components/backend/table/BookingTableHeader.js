95d58e31aba0595936e4f6f092f5aceb1408f744
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2021-02-24 22:45:42 +0700

    update

--- components/backend/table/BookingTableHeader.js
+++ components/backend/table/BookingTableHeader.js
@@ -5,26 +5,49 @@ import Datetime from 'react-datetime';
 
 
 const SubHeaderComponent = memo((props) => {
-  const {filterText, setFilterText, resetPaginationToggle, setResetPaginationToggle, placeholder} = props;
+  const {filterText, setFilterText, resetPaginationToggle, setResetPaginationToggle, placeholder, 
+    showstartDate, showendDate, validStartDate, valid, startDate, endDate, handleSearch} = props;
   const handleClear = () => {
     if (filterText) {
       setResetPaginationToggle(!resetPaginationToggle);
       setFilterText('');
     }
   };
-  return <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} placeholder={placeholder} />;
+  return <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} placeholder={placeholder}
+  showstartDate={showstartDate} showendDate={showendDate} validStartDate={validStartDate} valid={valid} handleSearch={handleSearch}
+  startDate={startDate} endDate={endDate} />;
 });
 
-const FilterComponent = ({ filterText, onFilter, onClear, placeholder }) => (
+const FilterComponent = ({ filterText, onFilter, onClear, placeholder, 
+  showstartDate, showendDate, validStartDate, valid, startDate, endDate, handleSearch }) => (
   <>
-    <div className="row w-100 justify-content-between">
-      <div className="col-lg-3 col-md-6 col-12 px-0">
-        <Datetime 
-        dateFormat="YYYY-MM-DD"
-        timeFormat={false}
-        /* onChange={(e)=> {showendDate(e)}}
-        value={endDate ? endDate : ''} */
-        inputProps={{ name: 'end_time', required : true, autoComplete : 'off' }} />
+    <div className="row w-100 justify-content-between align-items-center">
+      <div className="col-lg-3 col-md-6 col-12">
+        <div className="form-group">
+          <Datetime 
+          dateFormat="YYYY-MM-DD" 
+          timeFormat={false}
+          onChange={(e)=> {showstartDate(e)}}
+          value={startDate ? startDate : ''}
+          isValidDate={validStartDate}
+          inputProps={{ name: 'start_date', required : true, autoComplete : 'off' }} />
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-6 col-12">
+        <div className="form-group">
+          <Datetime 
+          dateFormat="YYYY-MM-DD" 
+          timeFormat={false}
+          onChange={(e)=> {showendDate(e)}}
+          value={endDate ? endDate : ''}
+          inputProps={{ name: 'end_date', required : true, autoComplete : 'off' }}
+          isValidDate={ valid }  />
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-6 col-12">
+        <div className="form-group">
+          <button type="button" className="btn btn-primary" onClick={() => handleSearch()}>Search by Date</button>
+        </div>
       </div>
       <div className="col-lg-3 col-md-6 col-12 px-0">
         <div className="d-flex">
