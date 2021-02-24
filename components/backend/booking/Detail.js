c8898730928b8ac37e6c34403c1924a070f2b7be
coldzify1 <39047015+coldzify1@users.noreply.github.com>
2021-02-24 23:42:36 +0700

    Update Detail.js

--- components/backend/booking/Detail.js
+++ components/backend/booking/Detail.js
@@ -8,6 +8,7 @@ const Detail = (props) => {
       <h5>Booking Details</h5>
       <p className="mb-1"><span>Booking NO. : </span> {data.booking_details[0]?.booking_id}</p>
       <p className="mb-1"><span>Package Name : </span> {data.booking_details[0]?.product?.name}</p>
+      <p className="mb-1"><span>Booking by : </span> {data.booking_by}</p>
     </div>
   )
 }
