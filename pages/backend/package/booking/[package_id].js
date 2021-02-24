6c76908a6cf9d01477c7d907688c1f589510899c
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2021-02-24 22:19:55 +0700

    update

--- pages/backend/package/booking/[package_id].js
+++ pages/backend/package/booking/[package_id].js
@@ -8,6 +8,8 @@ import SelectTime from '../../../../components/widget/SelectTime'
 import {toDateISO} from '../../../../utils/tools'
 import BookingAddress from '../../../../components/backend/package/BookingAddress'
 import Link from 'next/link'
+import Checkbox from '../../../../components/widget/Checkbox'
+import SelectLabel from '../../../../components/widget/SelectLabel';
 
 const Index = (props) => {
 
@@ -17,6 +19,8 @@ const Index = (props) => {
 
   const [activeFrom, setActiveFrom] = useState(false);
   const [activeTo, setActiveTo] = useState(false);
+  
+  const [companies, setCompany] = useState();
 
   const [state,setState] = useState({
     date : toDateISO(new Date()),
@@ -50,6 +54,24 @@ const Index = (props) => {
     fecthPackageOne();
   }, [package_id])
 
+
+  const fechCompany = () => {
+    api.getCompany()
+    .then(res=>{
+      const data = res.data;
+      var temp = data.map(val => ({...val,val : val.id})  )
+      setCompany(temp);
+    })
+    .catch(err => {
+      console.log(err.response);
+    })
+  }
+
+  useEffect(() => {
+    fechCompany();
+  },[]);
+ 
+
   const is_boat = true;
 
 
@@ -67,6 +89,45 @@ const Index = (props) => {
   const onTimeChange = (key, val) =>{
     setState({...state,[key] : val })
   }
+
+  const onAddonChange = (e) => {
+    var {name,checked} = e.target;
+    var id = name.split('-')[1]
+    if(!id) return;
+    var data = addons.find(val => val.id == id)
+    var tmp = [...state.addons]
+    let index = state.addons.findIndex(val => val.id == id)
+    if(checked){
+      if(index === -1){
+        tmp.push({...data,quantity : 1})
+      }
+    }
+    else{
+      if(index !== -1){
+        tmp.splice(index,1)
+      }
+    }
+    
+    setState({...state,addons:tmp})
+  }
+
+  const onAmountAddonChange = (name,quantity) =>{
+    var id = name.split('-')[1]
+    if(!id) return;
+    var tmp = [...state.addons]
+    var data = addons.find(val => val.id == id)
+    let index = state.addons.findIndex(val => val.id == id)
+    if(index === -1){
+      tmp.push({...data,quantity})
+    }
+    else{
+      tmp[index] = {...tmp[index],quantity}
+    }
+    
+    setState({...state,addons:tmp})
+  }
+
+  console.log(packages);
 
   return (
     <>
@@ -113,22 +174,62 @@ const Index = (props) => {
               </div>
               <div className="row mt-3">
                 <div className="col-3">
+                  <SelectLabel 
+                  inputProps={{ 
+                    className:'form-control', 
+                    name : 'company_type_id', required : true,
+                  }} 
+                  labelName="Company Type" icon={false} options={companies} />
+                </div>
+                <div className="col-3">
                   <InputLabel inputProps={{ 
-                    className:'form-control', type : 'text',
-                    name : 'adults', required : true
+                    className:'form-control', type : 'number',
+                    name : 'adults', required : true, min : 0
                   }} 
                   labelName="Adults  " iconProps={{className : 'fa icon icon-home'}} />
                 </div>
                 <div className="col-3">
                   <InputLabel inputProps={{ 
-                    className:'form-control', type : 'text',
-                    name : 'childrens', required : true
+                    className:'form-control', type : 'number',
+                    name : 'childrens', required : true, min : 0
                   }} 
                   labelName="Childrens " iconProps={{className : 'fa icon icon-home'}} />
                 </div>
               </div>
+              {
+                (packages.products_addons && packages.products_addons.length > 0) ? (
+                  <div className="row mt-4">
+                    <div className="col-4">
+                      <span>Addons</span>
+                      <div className="box-adddd mt-2">
+                        {
+                          packages.products_addons.map(val => 
+                            <Checkbox key={val.name} 
+                            name={`addon-${val.id}`} 
+                            checked={!!state.addons.find(item => item.id == val.id)}
+                            value1={val.name} 
+                            value2={parseInt(val.price) } 
+                            onChange={onAddonChange} 
+                            quantity={state.addons.find(item => item.id == val.id)?.quantity || 1}
+                            
+                            onAmountAddonChange={onAmountAddonChange} />
+                          )
+                        }
+                      </div>
+                    </div>
+                  </div>
+                ) : null
+              }
+              
               <div>
                 <BookingAddress />
+              </div>
+              <div className="row mt-4">
+                <div className="col-12">
+                  <div className="my-2">
+                    {<span className="font-24px">Net Price  ฿ </span>}
+                  </div>
+                </div>
               </div>
               <div className="row mt-4">
                 <div className="col-12">
