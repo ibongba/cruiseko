1ee63190adc47304e79d3c2734370c986f362566
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2021-03-12 10:22:25 +0700

    Update Footer.js

--- components/frontend/layout/Footer.js
+++ components/frontend/layout/Footer.js
@@ -6,35 +6,35 @@ const Footer = (props) => {
 
   return (
     <div className="footer">
-      <section class="Footer-links" data-selenium="footer-links-section">
-        <div class="LinksList container">
-          <div class="LinksListGroup LinksListGroup--vertical" data-selenium="footer-help-column">
-            <h5 class="LinksListGroup-title LinksListGroup-title--vertical">Help</h5>
-            <ul class="LinksListGroup-list">
-              <li class="LinksListLink"><a href="" target="_blank" class="LinksListLink-link" >Privacy policy</a></li>
-              <li class="LinksListLink"><a href="" target="_blank" class="LinksListLink-link" >Terms of use</a></li>
+      <section className="Footer-links" data-selenium="footer-links-section">
+        <div className="LinksList container">
+          <div className="LinksListGroup LinksListGroup--vertical" data-selenium="footer-help-column">
+            <h5 className="LinksListGroup-title LinksListGroup-title--vertical">Help</h5>
+            <ul className="LinksListGroup-list">
+              <li className="LinksListLink"><a href="" target="_blank" className="LinksListLink-link" >Privacy policy</a></li>
+              <li className="LinksListLink"><a href="" target="_blank" className="LinksListLink-link" >Terms of use</a></li>
             </ul>
           </div>
-          <div class="LinksListGroup LinksListGroup--vertical">
-            <h5 class="LinksListGroup-title LinksListGroup-title--vertical">About us</h5>
-            <ul class="LinksListGroup-list">
-              <li class="LinksListLink"><a href="" target="_blank" class="LinksListLink-link">Careers</a></li>
-              <li class="LinksListLink"><a href="" target="_blank" class="LinksListLink-link">Press</a></li>
-              <li class="LinksListLink"><a href="" target="_blank" class="LinksListLink-link" >Blog</a></li>
+          <div className="LinksListGroup LinksListGroup--vertical">
+            <h5 className="LinksListGroup-title LinksListGroup-title--vertical">About us</h5>
+            <ul className="LinksListGroup-list">
+              <li className="LinksListLink"><a href="" target="_blank" className="LinksListLink-link">Careers</a></li>
+              <li className="LinksListLink"><a href="" target="_blank" className="LinksListLink-link">Press</a></li>
+              <li className="LinksListLink"><a href="" target="_blank" className="LinksListLink-link" >Blog</a></li>
             </ul>
           </div>
-          <div class="LinksListGroup LinksListGroup--vertical">
-            <h5 class="LinksListGroup-title LinksListGroup-title--vertical">Destinations</h5>
-            <ul class="LinksListGroup-list">
-              <li class="LinksListLink"><a href="" class="LinksListLink-link">Countries/Territories</a></li>
-              <li class="LinksListLink"><a href="" class="LinksListLink-link">Cities</a></li>
+          <div className="LinksListGroup LinksListGroup--vertical">
+            <h5 className="LinksListGroup-title LinksListGroup-title--vertical">Destinations</h5>
+            <ul className="LinksListGroup-list">
+              <li className="LinksListLink"><a href="" className="LinksListLink-link">Countries/Territories</a></li>
+              <li className="LinksListLink"><a href="" className="LinksListLink-link">Cities</a></li>
             </ul>
           </div>
-          <div class="LinksListGroup LinksListGroup--vertical">
-            <h5 class="LinksListGroup-title LinksListGroup-title--vertical">Partner with us</h5>
-            <ul class="LinksListGroup-list">
-              <li class="LinksListLink"><a href="" class="LinksListLink-link">YCS partner portal</a></li>
-              <li class="LinksListLink"><a href="" class="LinksListLink-link">Partner solutions</a></li>
+          <div className="LinksListGroup LinksListGroup--vertical">
+            <h5 className="LinksListGroup-title LinksListGroup-title--vertical">Partner with us</h5>
+            <ul className="LinksListGroup-list">
+              <li className="LinksListLink"><a href="" className="LinksListLink-link">YCS partner portal</a></li>
+              <li className="LinksListLink"><a href="" className="LinksListLink-link">Partner solutions</a></li>
             </ul>
           </div>
         </div>
